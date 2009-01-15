0bcc72da95011e81b0d0dc2dfeee01367438772e
quan <quan@46651482-2d77-40f6-9cab-d3fc14350624>
2009-01-15 16:55:58 +0000

    include new block that shows latest successfull invitations

---
+++ pnlang/deu/latest.php
@@ -0,0 +1,7 @@
+<?php
+
+define('_INVITATION_OFUSERSTOSHOW',				'Maximale Anzahl der anzuzeigenden Benutzer');
+define('_INVITATION_HAS',						'hat');
+define('_INVITATION_INVITED',					'eingeladen');
+define('_INVITATION_THANKYOUFORRECOMMENDING',	'Danke, dass Ihr euere Freunde eingeladen habt!');
+define('_INVITATION_INVITEYOURFRIENDS',			'Lade auch Du Freunde ein!');
