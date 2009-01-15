8bcd8a5175481d3df2ba673dff866621687070c5
quan <quan@46651482-2d77-40f6-9cab-d3fc14350624>
2009-01-15 16:55:58 +0000

    include new block that shows latest successfull invitations

---
+++ Invitation/pnlang/eng/latest.php
@@ -0,0 +1,7 @@
+<?php
+
+define('_INVITATION_OFUSERSTOSHOW',				'Numbers of invitations to show');
+define('_INVITATION_HAS',						'has invited');
+define('_INVITATION_INVITED',					'');
+define('_INVITATION_THANKYOUFORRECOMMENDING',	'Thank you for inviting your friends!');
+define('_INVITATION_INVITEYOURFRIENDS',			'Invite your friends now!');
