a00fbec7b2671be5293c84e1b6fd3fb3d7b2909f
quan <quan@46651482-2d77-40f6-9cab-d3fc14350624>
2010-06-01 08:10:11 +0000

    preparing gettext conversion

--- pnlang/deu/latest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('_INVITATION_OFUSERSTOSHOW',				'Maximale Anzahl der anzuzeigenden Benutzer');
-define('_INVITATION_HAS',						'hat');
-define('_INVITATION_INVITED',					'eingeladen');
-define('_INVITATION_THANKYOUFORRECOMMENDING',	'Danke, dass Ihr neue Mitglieder geworben habt!');
-define('_INVITATION_INVITEYOURFRIENDS',			'Lade auch Du Freunde ein!');
--- pnlang/eng/latest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('_INVITATION_OFUSERSTOSHOW',				'Numbers of invitations to show');
-define('_INVITATION_HAS',						'has invited');
-define('_INVITATION_INVITED',					'');
-define('_INVITATION_THANKYOUFORRECOMMENDING',	'Thank you for inviting your friends!');
-define('_INVITATION_INVITEYOURFRIENDS',			'Invite your friends now!');
