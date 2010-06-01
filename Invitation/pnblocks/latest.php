8bb4140a1f0476d3af95f9542f098db0f8d62301
quan <quan@46651482-2d77-40f6-9cab-d3fc14350624>
2010-06-01 08:52:11 +0000

    adding meta information

--- Invitation/pnblocks/latest.php
+++ Invitation/pnblocks/latest.php
@@ -1,5 +1,14 @@
 <?php
 /**
+ * @package      Invitation
+ * @version      $Id$
+ * @author       Florian Schießl
+ * @link         http://www.ifs-net.de
+ * @copyright    Copyright (C) 2010
+ * @license      http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+
+/**
  * initialise block
  * 
  */
